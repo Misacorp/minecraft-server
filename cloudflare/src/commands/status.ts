9a0336a7ed5469329ca0db84f6e838ec42d7c69c
Misa Jokisalo <misa.jokisalo@knowit.fi>
2024-08-31 10:22:12 +0300

    Localize /status command

--- cloudflare/src/commands/status.ts
+++ cloudflare/src/commands/status.ts
@@ -8,6 +8,20 @@ import getMachineState from "../fly/getMachineState";
 import type { APIInteraction } from "discord-api-types/v10";
 import updateDiscordInteraction from "../discord/updateDiscordInteraction";
 import validateEnv from "../util/validateEnv";
+import { DISCORD_ROLES_WITH_PERMISSION } from "../discord/guilds";
+
+const messageContentLocalizations = {
+	fi: {
+		status: `Palvelimen tämänhetkinen tila on `,
+		error: `Jokin meni pieleen palvelimen tilaa haettaessa. Tässä on virheviesti: `,
+		defaultErrorMessage: `(virheviestiä ei ollut)`,
+	},
+	en: {
+		status: `The server's current state is `,
+		error: `Something went wrong when getting the server status. Here's the error message: `,
+		defaultErrorMessage: `(there was no error message)`,
+	},
+};
 
 /**
  * Handles the "check server status" command
@@ -46,21 +60,34 @@ const getFlyMachineState = async (
 		"DISCORD_APPLICATION_ID",
 	]);
 
+	// Determine which language to use based on the user's role (which server they're on)
+	// Default to English
+	let messageContent = messageContentLocalizations.en;
+
+	const memberRoles = interaction?.member?.roles ?? [];
+
+	// Finnish option
+	if (memberRoles.includes(DISCORD_ROLES_WITH_PERMISSION[0])) {
+		messageContent = messageContentLocalizations.fi;
+	}
+
 	try {
 		// Get the machine state
 		const state = await getMachineState(env.FLY_MACHINE_URI, env.FLY_API_TOKEN);
 
 		// Update the original Discord message with the machine's state
 		await updateDiscordInteraction(env.DISCORD_APPLICATION_ID, interaction, {
-			content: `The server's current state is \`${state.toUpperCase()}\``,
+			content: `${messageContent.status}\`${state.toUpperCase()}\``,
 		});
 	} catch (error: unknown) {
 		// Catch any errors and update the Discord user about them by editing the bot's original message.
 		const errorMessage =
-			error instanceof Error ? error.message : "(there was no error message)";
+			error instanceof Error
+				? error.message
+				: messageContent.defaultErrorMessage;
 
 		await updateDiscordInteraction(env.DISCORD_APPLICATION_ID, interaction, {
-			content: `Something went wrong when getting the server's status. Here's the error message: ${errorMessage}`,
+			content: `${messageContent.error}${errorMessage}`,
 		});
 	}
 };
